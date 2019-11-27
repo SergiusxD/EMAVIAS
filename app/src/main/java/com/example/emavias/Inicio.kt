0906c5c38dd54996472a0b78cb630ee3d42f5108
SergiusxD <sbellott@hotmail.com>
2019-11-27 15:08:05 -0400

    Initial commit

---
+++ app/src/main/java/com/example/emavias/Inicio.kt
@@ -0,0 +1,12 @@
+package com.example.emavias
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Inicio : AppCompatActivity() {
+
+   override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_inicio)
+}
+}
